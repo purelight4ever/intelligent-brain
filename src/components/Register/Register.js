2b6034fd188e5130e618407be3a58a62e1b06948
Essaid Serhani <Purelight4ever@gmail.com>
2018-05-03 13:06:21 -0400

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -30,7 +30,7 @@ class Register extends React.Component {
 			})
 		})
 		.then(response => {response.json()})
-		.then(JSON.user => {
+		.then(user => {
 			if (user.id) {
 				this.props.loadUser(user)
 				this.props.onRouteChange('home');
