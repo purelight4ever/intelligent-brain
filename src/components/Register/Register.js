199cc1205442fc3c8429bcfcd6057209eb4752bc
Essaid Serhani <Purelight4ever@gmail.com>
2018-05-03 12:33:19 -0400

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -29,7 +29,7 @@ class Register extends React.Component {
 				name: this.state.name
 			})
 		})
-		.then(response => response.json())
+		.then(response => {response.json()})
 		.then(user => {
 			if (user.id) {
 				this.props.loadUser(user)
