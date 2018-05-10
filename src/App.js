002a2bafbcb2da0319d337307dd3ca786af2d316
Essaid Serhani <Purelight4ever@gmail.com>
2018-05-10 16:22:05 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ class App extends Component {
 
   onButtonSubmit = (req, res) => {
     this.setState({imageUrl: this.state.input});
-    fetch('https://intelligent-brain.herokuapp.com/imageUrl', {
+    fetch('https://pure-sands-59281.herokuapp.com/imageUrl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
