dbb93005b2e8d1679e5f6cc95aba4bf6a5f08889
Essaid Serhani <Purelight4ever@gmail.com>
2018-05-10 16:05:04 -0400

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -16,7 +16,7 @@ class Signin extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('https://intelligent-brain.herokuapp.com/signin', {
+		fetch('https://pure-sands-59281.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
