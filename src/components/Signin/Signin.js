951c218ad1c512b9d9383491e4a5026c4ac1607e
Essaid Serhani <Purelight4ever@gmail.com>
2018-05-10 14:39:48 -0400

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -54,7 +54,7 @@ class Signin extends React.Component {
 			      <div className="mv3">
 			        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
 			        <input 
-			        className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+			        className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
 			        type="password" 
 			        name="password"  
 			        id="password" 
