93b496ca056acb4ecab3403dab6a92c7dad77eae
Essaid Serhani <Purelight4ever@gmail.com>
2018-05-10 14:28:41 -0400

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -71,7 +71,7 @@ class Signin extends React.Component {
 			      />
 			    </div>
 			    <div className="lh-copy mt3">
-			      <p onClick={() => onRouteChange('signin')}  className="f6 link dim black db pointer">Signin</p>
+			      <p onClick={() => onRouteChange('register')}  className="f6 link dim black db pointer">Register</p>
 			    </div>
 			  </div>
 			</main>
