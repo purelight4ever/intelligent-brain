f504050cd4b0def0c0e3a7bdb86f1586d6f0353c
Essaid Serhani <Purelight4ever@gmail.com>
2018-05-10 15:26:02 -0400

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -20,7 +20,7 @@ class Register extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch("https://pure-sands-59281.herokuapp.com/register", {
+		fetch("https://intelligent-brain.herokuapp.com/register", {
 			method: 'POST',
 			headers: {
 				'Accept':'application/json',
